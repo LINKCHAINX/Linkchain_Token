a1863fa6525fe946f527fc7e90eeaaef768103cf
Anshul Padyal <Anshulpadyal@users.noreply.github.com>
2018-09-03 14:25:39 +0530

    Create ERC20Basic.sol

---
+++ contracts/token/ERC20Basic.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) constant public returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
