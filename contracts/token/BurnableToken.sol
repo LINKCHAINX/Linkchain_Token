6ad3b35a52c780557adfb86c680b2087eff37306
Anshul Padyal <Anshulpadyal@users.noreply.github.com>
2018-09-05 12:02:39 +0530

    Create BurnableToken.sol
    Contract to burn tokens

---
+++ contracts/token/BurnableToken.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+import './token/StandardToken.sol'
+
+contract BurnableToken is StandardToken {
+
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint _value)
+        public
+    {
+        require(_value > 0);
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        emit Burn(burner, _value);
+    }
+
+    event Burn(address indexed burner, uint indexed value);
+}
